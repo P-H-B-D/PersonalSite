e312dce92b39b54f3b8a22ce87c5318124b158ef
P-H-B-D <98623538+P-H-B-D@users.noreply.github.com>
2023-08-08 17:23:42 -0400

    all windows

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var titleBarElemAbout= document.querySelector('.title-bar-about');
     var windowElemAbout = document.querySelector('.aboutWindow');
 
+    var titleBarElemStartups= document.querySelector('.title-bar-startups');
+    var windowElemStartups = document.querySelector('.startupsWindow');
+
+    var titleBarElemResearch= document.querySelector('.title-bar-research');
+    var windowElemResearch = document.querySelector('.researchWindow');
+
+    var titleBarElemProjects= document.querySelector('.title-bar-projects');
+    var windowElemProjects = document.querySelector('.projectsWindow');
+
+    var titleBarElemInterests= document.querySelector('.title-bar-interests');
+    var windowElemInterests = document.querySelector('.interestsWindow');
+
+
+    function preventDefaultBehavior(e) {
+        e.preventDefault();
+    }
+
+
     var offsetX, offsetY, isDragging = false;
     var currentDraggingElem = null; // This will keep track of which element we're dragging
 
@@ -22,13 +40,30 @@ document.addEventListener('DOMContentLoaded', function() {
         startDragging(event, windowElemAbout);
     });
 
-    
+    titleBarElemStartups.addEventListener('mousedown', function(event) {
+        startDragging(event, windowElemStartups);
+    });
+    titleBarElemResearch.addEventListener('mousedown', function(event) {
+        startDragging(event, windowElemResearch);
+    });
+    titleBarElemProjects.addEventListener('mousedown', function(event) {
+        startDragging(event, windowElemProjects);
+    });
+    titleBarElemInterests.addEventListener('mousedown', function(event) {
+        startDragging(event, windowElemInterests);
+    });
+
+    var currentZIndex = 0; // Initialize with 0 or with the highest z-index you have on your page.
 
     function startDragging(event, element) {
+        document.addEventListener('mousemove', preventDefaultBehavior);
         isDragging = true;
         currentDraggingElem = element;
         offsetX = event.clientX - element.getBoundingClientRect().left;
         offsetY = event.clientY - element.getBoundingClientRect().top;
+
+        currentZIndex++; // Increment the current z-index
+        currentDraggingElem.style.zIndex = currentZIndex; // Assign the incremented z-index to the dragged window
 
         document.addEventListener('mousemove', onMouseMove);
         document.addEventListener('mouseup', onMouseUp);
@@ -47,6 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function onMouseUp() {
         isDragging = false;
         currentDraggingElem = null;
+        document.removeEventListener('mousemove', preventDefaultBehavior);
 
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mouseup', onMouseUp);
@@ -66,16 +102,51 @@ document.addEventListener('DOMContentLoaded', function() {
         } 
         
     });
+
     document.querySelector('.close-btn').addEventListener('click', function() {
         windowElem.style.display = 'none';
     });
     document.querySelector('.close-btn-about').addEventListener('click', function() {
         windowElemAbout.style.display = 'none';
     });
+    document.querySelector('.close-btn-startups').addEventListener('click', function() {
+        windowElemStartups.style.display = 'none';
+    });
+    document.querySelector('.close-btn-research').addEventListener('click', function() {
+        windowElemResearch.style.display = 'none';
+    });
+    document.querySelector('.close-btn-projects').addEventListener('click', function() {
+        windowElemProjects.style.display = 'none';
+    });
+    document.querySelector('.close-btn-interests').addEventListener('click', function() {
+        windowElemInterests.style.display = 'none';
+    });
+
+
 
     document.getElementById("AboutText").addEventListener('click', function() {
         if (windowElemAbout.style.display === 'none' || !windowElemAbout.style.display) {
             windowElemAbout.style.display = 'block';
+        } 
+    });
+    document.getElementById("StartupsText").addEventListener('click', function() {
+        if (windowElemStartups.style.display === 'none' || !windowElemStartups.style.display) {
+            windowElemStartups.style.display = 'block';
+        } 
+    });
+    document.getElementById("ResearchText").addEventListener('click', function() {
+        if (windowElemResearch.style.display === 'none' || !windowElemResearch.style.display) {
+            windowElemResearch.style.display = 'block';
+        } 
+    });
+    document.getElementById("ProjectsText").addEventListener('click', function() {
+        if (windowElemProjects.style.display === 'none' || !windowElemProjects.style.display) {
+            windowElemProjects.style.display = 'block';
+        } 
+    });
+    document.getElementById("InterestsText").addEventListener('click', function() {
+        if (windowElemInterests.style.display === 'none' || !windowElemInterests.style.display) {
+            windowElemInterests.style.display = 'block';
         } 
     });
     
